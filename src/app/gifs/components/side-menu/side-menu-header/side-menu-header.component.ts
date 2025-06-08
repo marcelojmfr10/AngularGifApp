86ffe1140966eeb0bb30a39552dc267d9430c320
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-06-08 14:42:40 -0600

    División de plantilla de tailwindcss en componentes y RouterLinks

---
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './side-menu-header.component.html',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideMenuHeaderComponent {
+
+
+}
---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu-options.component.html',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ];
+
+}
